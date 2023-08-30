c01bd56fb23e80243fec3ed4178a0bd9e583c3ee
Mohammad Hossein Amirpanahi <118535877+mhap75@users.noreply.github.com>
2023-08-30 09:59:10 +0330

    Develop Tech

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { technologies } from "../const/index.js";
+import { SectionWrapper } from "../hoc/index.js";
+import { BallCanvas } from "./canvas/index.js";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flexWrap justify-center gap-10">
+      {technologies.map(({ name, icon }) => (
+        <div key={name}>
+          <div className="h-28 w-28">
+            <BallCanvas icon={icon} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
